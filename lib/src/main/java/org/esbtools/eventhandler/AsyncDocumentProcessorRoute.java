fc74901b18bb2849f2ed701e641f1447809d02b4
Alec Henninger <ahenning@redhat.com>
2016-02-11 15:30:15 -0500

    Introduce DocumentRepository for processing Document messages

---
+++ lib/src/main/java/org/esbtools/eventhandler/AsyncDocumentProcessorRoute.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2016 esbtools Contributors and/or its affiliates.
+ *
+ *  This file is part of esbtools.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.esbtools.eventhandler;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class AsyncDocumentProcessorRoute extends RouteBuilder {
+    private final DocumentRepository documentRepository;
+    private final String fromUri;
+
+    public AsyncDocumentProcessorRoute(DocumentRepository documentRepository, String fromUri) {
+        this.documentRepository = documentRepository;
+        this.fromUri = fromUri;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from(fromUri)
+                .process(exchange -> {
+                   documentRepository.insertOrUpdate(exchange.getIn().getBody());
+                });
+    }
+}
