54cfcf84814504ae7ea26315ccbca2b470f52bb6
Alec Henninger <ahenning@redhat.com>
2016-03-30 18:13:29 -0400

    Test retry failed message route

--- lib/src/main/java/org/esbtools/eventhandler/RetryingBatchFailedMessageRoute.java
+++ lib/src/main/java/org/esbtools/eventhandler/RetryingBatchFailedMessageRoute.java
@@ -36,9 +36,9 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class RetryingFailedMessageRoute extends RouteBuilder {
+public class RetryingBatchFailedMessageRoute extends RouteBuilder {
     private final String fromUri;
-    private final Expression retryDelay;
+    private final Expression retryDelayMillis;
     private final int maxRetryCount;
     private final Duration processTimeout;
     private final String deadLetterUri;
@@ -46,10 +46,10 @@ public class RetryingFailedMessageRoute extends RouteBuilder {
     private static final String NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY = "eventHandlerFailedMessageNextRetryAttemptNumber";
     private static final Integer FIRST_RETRY_ATTEMPT_NUMBER = 1;
 
-    public RetryingFailedMessageRoute(String fromUri, Expression retryDelay, int maxRetryCount,
+    public RetryingBatchFailedMessageRoute(String fromUri, Expression retryDelayMillis, int maxRetryCount,
             Duration processTimeout, String deadLetterUri) {
         this.fromUri = fromUri;
-        this.retryDelay = retryDelay;
+        this.retryDelayMillis = retryDelayMillis;
         this.maxRetryCount = maxRetryCount;
         this.processTimeout = processTimeout;
         this.deadLetterUri = deadLetterUri;
@@ -59,106 +59,132 @@ public class RetryingFailedMessageRoute extends RouteBuilder {
     public void configure() throws Exception {
         from(fromUri)
         // We use loop instead of error handler because error handlers start with original message
-        // sent to point of failure. We need to continuously process until there are no more
-        // remaining failures. We don't want to reprocess messages which already succeeded.
+        // sent to point of failure; we need the message to stay intact to prevent reprocessing
+        // already succeeded messages and to keep context of previous tries' failures.
         .loopDoWhile(and(
                 exchangeHasFailures(),
                 not(maxRetryCountMet())))
-            .delay(retryDelay)
-            .process(exchange -> {
-                Integer retryAttempt = Optional.of(
-                        exchange.getProperty(NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY, Integer.class))
-                        .orElse(FIRST_RETRY_ATTEMPT_NUMBER);
-                // Preemptively increment retry attempt for next loop.
-                exchange.setProperty(NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY, retryAttempt + 1);
+            .delay(retryDelayMillis)
+                // Indenting because delay actually starts a child processor, and needs its own end()
+                // See: https://issues.apache.org/jira/browse/CAMEL-2654
+                .process(exchange -> {
+                    Integer retryAttempt = Optional.ofNullable(
+                            exchange.getProperty(NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY, Integer.class))
+                            .orElse(FIRST_RETRY_ATTEMPT_NUMBER);
+                    // Preemptively increment retry attempt for next loop.
+                    exchange.setProperty(NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY, retryAttempt + 1);
 
-                Collection oldFailures = exchange.getIn().getMandatoryBody(Collection.class);
-                List<FailedMessage> newFailures = new ArrayList<>();
-                List<ReprocessingFailure> reprocessingFailures = new ArrayList<>(oldFailures.size());
+                    Collection oldFailures = exchange.getIn().getMandatoryBody(Collection.class);
 
-                // Begin processing all failed messages again in parallel.
-                for (Object failureAsObject : oldFailures) {
-                    if (!(failureAsObject instanceof FailedMessage)) {
-                        throw new IllegalArgumentException("Messages sent to " + RetryingFailedMessageRoute.class +
-                                "route should be collections of FailedMessage elements, but got " +
-                                "collection of " + failureAsObject.getClass());
+                    List<FailedMessage> newFailures = new ArrayList<>();
+                    List<ReprocessingFailure> reprocessingFailures =
+                            new ArrayList<>(oldFailures.size());
+
+                    // Begin processing all failed messages again in parallel.
+                    for (Object failureAsObject : oldFailures) {
+                        if (!(failureAsObject instanceof FailedMessage)) {
+                            throw new IllegalArgumentException("Messages sent to " +
+                                    RetryingBatchFailedMessageRoute.class + " route should be " +
+                                    "collections of FailedMessage elements, but got collection " +
+                                    "of " + failureAsObject.getClass());
+                        }
+
+                        FailedMessage failure = (FailedMessage) failureAsObject;
+                        Optional<Message> maybeMessage = failure.parsedMessage();
+
+                        if (!maybeMessage.isPresent()) {
+                            // Nothing to retry; dead letter it
+                            // This happens when message factory failed to get message from original
+                            // body. We won't bother trying get the message from the message factory
+                            // again; if that fails it is usually a bug that retrying won't circumvent.
+                            continue;
+                        }
+
+                        Message message = maybeMessage.get();
+                        final Future<Void> reprocessingFuture;
+
+                        try {
+                            reprocessingFuture = message.process();
+                        } catch (Exception e) {
+                            log.error("Failed to reprocess message (retry attempt " +
+                                    retryAttempt + "): " + message, e);
+                            suppressPreviousFailureInNewException(failure, e);
+                            newFailures.add(new FailedMessage(failure.originalMessage(), message, e));
+                            continue;
+                        }
+
+                        reprocessingFailures.add(new ReprocessingFailure(failure, reprocessingFuture));
                     }
 
-                    FailedMessage failure = (FailedMessage) failureAsObject;
-                    Optional<Message> maybeMessage = failure.parsedMessage();
+                    for (ReprocessingFailure reprocessingFailure : reprocessingFailures) {
+                        FailedMessage originalFailure = reprocessingFailure.originalFailure;
 
-                    if (!maybeMessage.isPresent()) {
-                        // Nothing to retry; dead letter it
-                        // This happens when message factory failed to get message from original body.
-                        // We won't bother trying get the message from the message factory again; if
-                        // that fails it is usually a bug that retrying won't circumvent.
-                        continue;
+                        try {
+                            reprocessingFailure.reprocessingFuture
+                                    .get(processTimeout.toMillis(), TimeUnit.MILLISECONDS);
+                        } catch (ExecutionException e) {
+                            Message parsedMessage = originalFailure.parsedMessage().get();
+
+                            log.error("Failed to reprocess message (retry attempt " + retryAttempt +
+                                    "): " + parsedMessage, e);
+
+                            Throwable realException = e.getCause();
+                            suppressPreviousFailureInNewException(originalFailure, realException);
+
+                            FailedMessage failure = new FailedMessage(
+                                    originalFailure.originalMessage(), parsedMessage, realException);
+                            newFailures.add(failure);
+                        } catch (InterruptedException | TimeoutException e) {
+                            Message parsedMessage = originalFailure.parsedMessage().get();
+
+                            log.warn("Timed out reprocessing message (retry attempt " + retryAttempt +
+                                    "): " + parsedMessage, e);
+
+                            suppressPreviousFailureInNewException(originalFailure, e);
+
+                            // TODO(ahenning): Consider removing recoverable exception thing
+                            RecoverableException recoverableException = new RecoverableException(e);
+
+                            FailedMessage failure = new FailedMessage(originalFailure.originalMessage(),
+                                    parsedMessage, recoverableException);
+                            newFailures.add(failure);
+                        }
                     }
 
-                    Message message = maybeMessage.get();
-                    final Future<Void> reprocessingFuture;
-
-                    try {
-                        reprocessingFuture = message.process();
-                    } catch (Exception e) {
-                        log.error("Failed to reprocess message (retry attempt " + retryAttempt + "): " + message, e);
-                        // Track the previous try's exception.
-                        e.addSuppressed(failure.exception());
-                        newFailures.add(new FailedMessage(failure.originalMessage(), message, e));
-                        continue;
-                    }
-
-                    reprocessingFailures.add(new ReprocessingFailure(failure, reprocessingFuture));
-                }
-
-                for (ReprocessingFailure reprocessingFailure : reprocessingFailures) {
-                    FailedMessage originalFailure = reprocessingFailure.originalFailure;
-
-                    try {
-                        reprocessingFailure.reprocessingFuture
-                                .get(processTimeout.toMillis(), TimeUnit.MILLISECONDS);
-                    } catch (ExecutionException e) {
-                        Message parsedMessage = originalFailure.parsedMessage().get();
-
-                        log.error("Failed to reprocess message (retry attempt " + retryAttempt +
-                                "): " + parsedMessage, e);
-
-                        // Track the previous try's exception.
-                        e.getCause().addSuppressed(originalFailure.exception());
-
-                        FailedMessage failure = new FailedMessage(originalFailure.originalMessage(),
-                                parsedMessage, e.getCause());
-                        newFailures.add(failure);
-                    } catch (InterruptedException | TimeoutException e) {
-                        Message parsedMessage = originalFailure.parsedMessage().get();
-
-                        // Track the previous try's exception.
-                        e.getCause().addSuppressed(originalFailure.exception());
-
-                        log.warn("Timed out reprocessing message (retry attempt " + retryAttempt +
-                                "): " + parsedMessage, e);
-
-                        // TODO(ahenning): Consider removing recoverable exception thing
-                        e.addSuppressed(originalFailure.exception());
-                        RecoverableException recoverableException = new RecoverableException(e);
-                        FailedMessage failure = new FailedMessage(originalFailure.originalMessage(),
-                                parsedMessage, recoverableException);
-                        newFailures.add(failure);
-                    }
-                }
-
-                // Give new failures another shot or dead letter them.
-                exchange.getIn().setBody(newFailures);
-            })
-        .end()
+                    // Give new failures another shot or dead letter them.
+                    exchange.getIn().setBody(newFailures);
+                })
+            .end() // end delay -- see comment below delay(...).
+        .end() // end loop
+        // If we still have failures, dead letter them.
+        .filter(exchangeHasFailures())
         .to(deadLetterUri);
+    }
+
+    /**
+     * In the event a messages fails on subsequent retries, this tracks that previous failure as a
+     * suppressed exception in the latest failure, keeping the history of failures for debugging.
+     *
+     * <p>Makes sure the exceptions are not referring to the same object to avoid a infinite
+     * recursion.
+     */
+    private void suppressPreviousFailureInNewException(FailedMessage failure, Throwable e) {
+        Throwable previousException = failure.exception();
+
+        if (e != previousException) {
+            e.addSuppressed(previousException);
+
+            for (Throwable previousSuppressed : previousException.getSuppressed()) {
+                e.addSuppressed(previousSuppressed);
+            }
+        }
     }
 
     private Predicate maxRetryCountMet() {
         return new Predicate() {
             @Override
             public boolean matches(Exchange exchange) {
-                Integer retryAttempt = Optional.of(
+                Integer retryAttempt = Optional.ofNullable(
                         exchange.getProperty(NEXT_RETRY_ATTEMPT_NUMBER_PROPERTY, Integer.class))
                         .orElse(FIRST_RETRY_ATTEMPT_NUMBER);
 
